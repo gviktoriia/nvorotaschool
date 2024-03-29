62a86ff030cb1115bca650d19792fe9f4cf49872
gviktoriia <gviktoria10@ukr.net>
2024-03-29 14:26:53 +0200

    SEO optimization started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
 import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Нижньоворітський ЗЗСО І-ІІІ Ступенів",
+  description:
+    "Провідний заклад загальної середньої освіти в с. Нижні Ворота Нижньоворітської ОТГ Мукачівського району Закарпатської області.",
+  keywords: ["Нижньоворітський ЗЗСО", "Нижні Ворота", "школа"],
+  icons: { icon: "./Logo School 2.png" },
+};
 
 export default function Home() {
   return (
@@ -11,12 +20,10 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <h2 className={`mb-3 text-4xl font-semibold`}>
-          Сайт тимчасово в розробці
+          Сайт в процесі розробки
         </h2>
       </div>
-      <div className="loader">
-       
-      </div>
+      <div className="loader"></div>
     </main>
   );
 }
