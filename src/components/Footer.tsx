c89d8db4735bac04deb3fec9b473885c77bd423e
gviktoriia <gviktoria10@ukr.net>
2024-04-08 02:06:05 +0300

    flex-wrap in stats

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section
+      className="max-container padding-container flex flex-col justify-center items-center relative"
+      style={{
+        backgroundColor: "#000000",
+      }}
+    >
+      <p className="text-white">Copyright © 2024 Нижньоворітський ЗЗСО</p>
+    </section>
+  );
 };
 
 export default Footer;
--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -9,20 +9,20 @@ const Statistics = () => {
         marginTop: "86vh",
       }}
     >
-      <div className="flex row gap-x-40 mt-16 mb-16">
-        <div className="text-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center gap-x-32 gap-y-32 mt-16 mb-16 mr-5 ml-5">
+        <div className="text-center w-full sm:w-1/4">
           <p className="text-white bold-40 mb-3">185</p>
           <p className="text-white regular-18">Учнів</p>
         </div>
-        <div className="text-center">
-          <p className="text-white bold-40 mb-3">23</p>
+        <div className="text-center w-full sm:w-1/4">
+          <p className="text-white bold-40 mb-3 ">23</p>
           <p className="text-white regular-18">Педагогів</p>
         </div>
-        <div className="text-center">
+        <div className="text-center w-full sm:w-1/4">
           <p className="text-white bold-40 mb-3">22098904</p>
           <p className="text-white regular-18">Код ЄДРПОУ</p>
         </div>
-        <div className="text-center">
+        <div className="text-center w-full sm:w-1/4">
           <p className="text-white bold-40 mb-3">1950</p>
           <p className="text-white regular-18">Рік заснування</p>
         </div>
